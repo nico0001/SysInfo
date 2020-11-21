f54d441d95b4dadad8d0141694f385d57fe206c9
saskia3636 <saskia.juffern@student.uclouvain.be>
2020-11-21 15:52:15 +0100

    graphes.py marche

--- graphes.py
+++ graphes.py
@@ -11,48 +11,42 @@ stdPhilo = []
 for i in range(0,len(philoData),15) :
 	n = philoData[i: i+15]
 	philoAverages.append(np.mean(n))
-stdPhilo = np.std(n)
-print(philoAverages)
-print(stdPhilo)
-plt.subplot(311)
+	stdPhilo.append(np.std(n))
+
 plt.xlabel('Nombre des coeurs')
 plt.ylabel("Temps moyenne d'execution")
-plt.title('Temps avec différentes coeurs')
-#plt.plot(range(2,len(philoData)//15+2),philoAverages)
-plt.errorbar(range(2,len(philoData)//15+2),philoAverages, yerr='stdPhilo')
-
+plt.title("Temps d'execution avec différentes coeurs")
+plt.errorbar(range(2,len(philoData)//15+2),philoAverages, yerr=stdPhilo)
+plt.axis(ymin=0)
+plt.savefig("Philo.png")
+plt.show()
 
 prodAverages = []
+stdProd = []
 for i in range(0,len(prodConsData),5) :
 	n = prodConsData[i: i+5]
 	prodAverages.append(np.mean(n))
-plt.subplot(312)
-plt.plot(range(2,len(prodConsData)//5+2),prodAverages)
+	stdProd.append(np.std(n))
+
+plt.xlabel('Nombre des coeurs')
+plt.ylabel("Temps moyenne d'execution")
+plt.title("Temps d'execution avec différentes coeurs")
+plt.errorbar(range(2,len(prodConsData)//5+2),prodAverages, yerr=stdProd)
+plt.axis(ymin=0)
+plt.savefig("Prod.png")
+plt.show()
 
 lectAverages = []
+stdLect = []
 for i in range(0,len(lectEcrvData),5) :
 	n = lectEcrvData[i: i+5]
 	lectAverages.append(np.mean(n))
-plt.subplot(313)
-plt.plot(range(2,len(lectEcrvData)//5+2),lectAverages)
+	stdLect.append(np.std(n))
 
+plt.xlabel('Nombre des coeurs')
+plt.ylabel("Temps moyenne d'execution")
+plt.title("Temps d'execution avec différentes coeurs")
+plt.errorbar(range(2,len(lectEcrvData)//5+2),lectAverages, yerr = stdLect)
+plt.axis(ymin=0)
+plt.savefig("Lect.png")
 plt.show()
-
-
-'''i=0
-averages = []
-
-while i < 35:
-	n = data['elapsed_time'][i: i+5]
-	averages.append(np.mean(n) / 5)
-	i = i+5
-
-print(averages)
-figure = plt.figure()
- 
-
-
-plt.savefig("exo.png")
-plt.show()
-plt.close()
-'''
